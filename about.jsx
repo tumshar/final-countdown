46e9e08d128c2d296beb2be68a518d2fc8e4ccd4
tumshar <88685603+tumshar@users.noreply.github.com>
2024-07-02 11:03:19 +0530

    Add files via upload

---
+++ about.jsx
@@ -0,0 +1,109 @@
+const About=()=> {
+
+    return(
+        <div>
+            <h1> About</h1>
+            <h2>  Seedhe Maut
+$ Sign One Time
+Gazipur ka khatta meri pehchaan
+Pehchaan mujhe
+Tere jungle ka mai sher khan
+Zeher kaam karu na le kisi ka bhi ehsaan
+Baap bane mera teri maa yaha pe meherbaan
+King sized double bed
+East side trouble hai
+Base aur treble hai
+Hatiyar FL hai
+Feel like kaif
+Teri feilding galat hai
+Aandu akal ke
+Hero bane magar hai shakal se lund
+Launde rubber hai
+Saste, sadar (bazar) hai
+Ghiste rOz
+Lekin akal hai kum Pengame marham hai
+Ek haath mic aur
+Ek haat kalam hai
+Bitch im the one
+Jaise neo Khatte ki baas me tu saans mt liyo
+Yaha launde dagabaz In short, matt peeyo
+Tujhe dera koi free, tera sab cheen lenge meri jaan
+Pata mujhe sab kyuki ghar hai dairy farm
+Bike Smack, bike doodh Yaha bike har samaan
+Bike jaan bhi koi yaha pe agar lagade sahi daam
+Bike jaan bhi koi yaha pe agar lagade sahi daam
+Pehchaan mujhe
+Tere jungle ka mai sher khan
+Zeher kaam karu na le kisi ka bhi ehsaan
+Baap bane mera teri maa yaha pe
+Tujhe pata hai kya hai meri pehchan
+Nainital Gaane fatte Siyahi me tha napalm
+Neymar jab bhi karu ball hu me way par Shaitaan
+Bole jinhe samajh aata paigaam
+Chakku double edge
+Fattu muggle hai
+Fuck you struggle
+Aaj tub me bubble hai
+Bande chatak hain
+Bechte kabar hain
+Bokhlau Bandi ki 24 kamar hai
+Bohot Karu stunt real life jaise jackie chan
+And make her cum again
+Likhu With a butter pen
+Seedhe maut shippuden
+Ye Pehne mamypoko pant
+Aati itti deale Ki Na mile kabhi Mo ko chain
+Bitch im the one jaise jett lee
+You dont wanna test me
+Dost rakhe blade jaise wesley
+Trust me yaha Skills aur dreams ki bane chutney to
+Industry bhi jaane ki ye boys Dete Fuck ni
+Chaapu kagaaz poora focused Kahenge kya ye bande
+Gobar Dum pishaach do game k top par
+Kabhi ni bhoole aaye kaha se kaha par
+Gazipur ka khatta meri pehchaan
+Pehchaan mujhe
+Tere jungle ka mai sher khan
+Zeher kaam karu na le kisi ka bhi ehsaan
+Baap bane mera teri maa yaha pe mehrban
+Tujhe pata hai kya hai meri pehchan
+Nainital Gaane fatte Siyahi me tha napalm
+Neymar jab bhi karu ball hu me way par Shaitaan
+Bole jinhe samajh aata paigaam
+Pehchaan kaise choda inhe hairaan
+Gurugraam kara mene paida pura maidaan
+Gairkan'ooni kare kaam Na bole jai ram
+Ghoome mere fan har jagah jaise khaitaan
+Fair chance, mila nahi
+Sehta guna nahi
+Tryna pull strings
+Tu na Ehsaan noorani
+Hai Kaun juwari
+Eehsaan chuka ni
+Dekh bani tere bhai ke lehr aaj tsunami
+A killer with the venom I'm getting Better with the program
+Saare competitor mere bete yaha, gohan
+Me hoo chin checker Na karoo Settle with no bank
+Mera Vendetta, being rapping better than most can
+Pooch rahe sawal ab, Kar le Wakalat
+Bande kamal Toh karte bawal
+Ab Sabke jawab hai, Rakhte zuban ab Pakde bars
+Hum na bharte zamanat
+Bole kitna wavy
+Said bitch it's Fuck you, pay me
+John wick hai with a weapon
+Bout to kill 'em parabellum
+Sare din hum dekhe panic in them, manic n i'm crazy
+Gazipur ka khatta meri pehchaan
+Pehchaan mujhe
+Tere jungle ka mai sher khan
+Zeher kaam karu na le kisi ka bhi ehsaan
+Baap bane mera teri maa yaha pe </h2>
+
+
+
+        </div>
+    );
+};
+
+export default About;
