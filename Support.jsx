f7add2804c2e1bc5733ce0269239002c5420c0d8
tumshar <88685603+tumshar@users.noreply.github.com>
2024-07-03 05:47:38 +0530

    Add files via upload

---
+++ Support.jsx
@@ -0,0 +1,99 @@
+ const Support =()=>{
+
+    return (
+        <div>
+            <h1> Support Page</h1>
+            I got, I got, I got, I got<br />
+    Loyalty, got royalty inside my DNA<br />
+    Quarter piece, got war, and peace inside my DNA<br />
+    I got power, poison, pain, and joy inside my DNA<br />
+    I got hustle, though, ambition flow inside my DNA<br />
+    I was born like this<br />
+    Since one like this, immaculate conception<br />
+    I transform like this, perform like this<br />
+    Was Yeshua new weapon<br />
+    I don't contemplate, I meditate<br />
+    Then off your-, off your head<br />
+    This that put-the-kids-to-bed<br />
+    This that I got, I got, I got, I got<br />
+    Realness, I just kill sh- 'cause it's in my DNA<br />
+    I got millions, I got riches buildin' in my DNA<br />
+    I got dark, I got evil that rot inside my DNA<br />
+    I got off, I got troublesome heart inside my DNA<br />
+    I just win again, then, win again like Wimbledon I serve<br />
+    Yeah, that's him again, the sound that engine in is like a bird<br />
+    You see fireworks and Corvette tire skrrt the boulevard<br />
+    I know how you work, I know just who you are<br />
+    See, you's a, you's a, you's a<br />
+    B-, your hormones prolly switch inside your DNA<br />
+    Problem is, all that sucker shit inside your DNA<br />
+    Daddy prolly snitched, heritage inside your DNA<br />
+    Backbone don't exist, born outside a jellyfish, I gauge<br />
+    See, my pedigree most definitely don't tolerate the front<br />
+    Sh- I've been through prolly offend you<br />
+    This is Paula's oldest son<br />
+    I know murder, conviction, burners, boosters<br />
+    Burglars, ballers, dead, redemption<br />
+    Scholars, fathers dead with kids, and<br />
+    I wish I was fed forgiveness<br />
+    Yeah, yeah, yeah, yeah, soldier's DNA (I'ma soldier DNA)<br />
+    Born inside the beast<br />
+    My expertise checked out in second grade<br />
+    When I was nine, on cell, motel<br />
+    We didn't have nowhere to stay<br />
+    At 29, I've done so well, hit cartwheel in my estate<br />
+    And I'm gon' shine like I'm supposed to antisocial, extrovert<br />
+    And excellent mean the extra work<br />
+    And absentness what the fuck you heard<br />
+    And pessimists never struck my nerve<br />
+    And Nazareth gonna plead this case<br />
+    The reason my power's here on earth<br />
+    Salute the truth, when the prophet say<br />
+    I-I got loyalty, got royalty inside my DNA (this is why I say that hip hop)<br />
+    (Has done more damage to young African Americans) (I got loyalty, got royalty inside my DNA)<br />
+    I live a better life, I'm rollin' several dice, f- your life<br />
+    (Than racism in recent years) (I got loyalty, got royalty inside my DNA)<br />
+    (I live a be-, - your life)<br />
+    This is my heritage, all I'm inheritin' (five, four, three, two, one)<br />
+    Money and power, the Mecca of marriages<br />
+    Tell me somethin' (give me some ganja)<br />
+    You motherfuckers can't tell me nothin'<br />
+    I'd rather die than to listen to you<br />
+    My DNA not for imitation, your DNA, an abomination<br />
+    This how it is when you're in the Matrix<br />
+    Dodgin' bullets, reapin' what you sow<br />
+    And stackin' up the footage, livin' on the go, and sleepin' in a villa<br />
+    Sippin' from a Grammy and walkin' in the buildin'<br />
+    Diamond in the ceilin', marble on the floors<br />
+    Beach inside the window, peekin' out the window<br />
+    Baby in the pool, godfather goals<br />
+    Only Lord knows I've been goin' hammer<br />
+    Dodgin' paparazzi, freakin' through the cameras<br />
+    Eat at Four Daughters, Brock wearin' sandals<br />
+    Yoga on a Monday, stretchin' to Nirvana<br />
+    Watchin' all the snakes, curvin' all the fakes<br />
+    Phone never on, I don't conversate<br />
+    I don't compromise, I just penetrate<br />
+    Sex, money, murder, these are the breaks<br />
+    These are the times, level number nine<br />
+    Look up in the sky, .10 is on the way<br />
+    Sentence on the way, killings on the way<br />
+    Motherf-ker, I got winners on the way<br />
+    You ain't sh- without a body on your belt<br />
+    You ain't sh- without a ticket on your plate<br />
+    You ain't sick enough to pull it on yourself<br />
+    You ain't rich enough to hit the lot and skate<br />
+    Tell me when destruction gonna be my fate<br />
+    Gonna be your fate, gonna be our faith?<br />
+    Peace to the world, let it rotate<br />
+    Sex, money, murder, our DNA<br />
+
+
+
+
+
+        </div>
+    );
+ };
+
+ export default Support;
