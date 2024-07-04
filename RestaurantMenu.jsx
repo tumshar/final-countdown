44ec1c6132e282f222e89fa83d4d1c5101ebfded
tumshar <88685603+tumshar@users.noreply.github.com>
2024-07-04 06:04:28 +0530

    Add files via upload

---
+++ RestaurantMenu.jsx
@@ -0,0 +1,17 @@
+
+const RestaurantMenu=()=> {
+    return (
+        <div className="menu">
+            <h1> Name of the Restaurant</h1>
+            <h2> Menu</h2>
+            <ul>
+                <li>Biryani</li>
+                <li>Burger</li>
+                <li>Pizza</li>
+                <li>Pasta</li>
+            </ul>
+        </div>
+    );
+};
+
+export default RestaurantMenu;
