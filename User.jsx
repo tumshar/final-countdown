087eeb4f455cb467ce0af23489cc3ad94a98ed52
tumshar <88685603+tumshar@users.noreply.github.com>
2024-07-06 19:08:20 +0530

    Add files via upload

---
+++ User.jsx
@@ -0,0 +1,13 @@
+const User = ()=> {
+    return (
+        <div className="user-card">
+            <h2> Name : tumshar</h2>
+            <h3> location : Banswara</h3>
+            <h3> contact : i am everywhere</h3>
+
+            
+             </div>
+    );
+};
+
+export default User;
